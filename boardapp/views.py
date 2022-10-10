2ca5d6857fa4e60af264dfee5345980672c7354c
mizki <wwr8462@gmail.com>
2022-10-10 13:17:19 +0900

    ログイン機能の実装

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import BoardModel
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -26,13 +27,14 @@ def loginfunc(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request, "login.html", {"context": "logged in"})
+            return redirect('list')
 
         else:
-            return render(request, "login.html", {"context": "not logged in"})
-    return render(request, "login.html", {"context": "get method"})
+            return render(request, "login.html", {})
+    return render(request, "login.html", {})
 
 
+# @login_required
 def listfunc(request):
     object_list = BoardModel.objects.all()
     return render(request, 'list.html', {'object_list':object_list})
--- boardproject/settings.py
+++ boardproject/settings.py
@@ -129,6 +129,8 @@ STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
