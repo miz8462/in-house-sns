931e55b373d2b4622951f240b8038a2e36687bba
mizki <wwr8462@gmail.com>
2022-10-10 09:03:00 +0900

    投稿モデルの作成

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,31 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render
 
 # Create your views here.
 
 
-def signupfunc(request): # 登録
+def signupfunc(request):  # 登録
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST["username"]
+        password = request.POST["password"]
         try:
-            user = User.objects.create_user(username, '', password)
-            return render(request, 'signup.html', {'some': 100})
+            user = User.objects.create_user(username, "", password)
+            return render(request, "signup.html", {"some": 100})
         except IntegrityError:
-            return render(request, 'signup.html', {'error': 'このユーザーはすでに登録されています'})
-    return render(request, 'signup.html', {'some': 100})
+            return render(request, "signup.html", {"error": "このユーザーはすでに登録されています"})
+    return render(request, "signup.html", {"some": 100})
+
 
 def loginfunc(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST["username"]
+        password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request, 'signup.html', {'context': 'logged in'})
+            return render(request, "login.html", {"context": "logged in"})
 
         else:
-            return render(request, 'signup.html', {'context': 'not logged in'})
-    return render(request, 'signup.html', {'context': 'get method'})
+            return render(request, "login.html", {"context": "not logged in"})
+    return render(request, "login.html", {"context": "get method"})
+
+
+def listfunc(request):
+    return render(request, 'list.html', {})
